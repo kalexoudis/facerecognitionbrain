7006ee64607c342c59f632a78158cfd92cda483a
kalexoudis <kons98@aol.com>
2020-04-18 11:52:58 +0200

    finished front-end

--- src/Components/Signin/SignIn.js
+++ src/Components/Signin/SignIn.js
@@ -4,7 +4,7 @@ const SignIn = ({onRouteChange}) => {
     return (
         <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                         <div className="mt3">
@@ -24,10 +24,10 @@ const SignIn = ({onRouteChange}) => {
                     <div className="lh-copy mt3">
                         <p className="f6 link dim black db pointer" onClick={() => onRouteChange("register")}>Register</p>
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     );
-}
+};
 
 export default SignIn;
